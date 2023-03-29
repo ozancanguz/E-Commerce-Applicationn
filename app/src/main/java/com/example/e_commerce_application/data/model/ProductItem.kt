23a54d0ee7805565038582d51949841b39f079b2
Ozan <ozancangu24@gmail.com>
2023-03-29 18:26:44 +0300

    -product data class created

---
+++ app/src/main/java/com/example/e_commerce_application/data/model/ProductItem.kt
@@ -0,0 +1,9 @@
+package com.example.e_commerce_application.data.model
+
+data class ProductItem(
+    val description: String,
+    val id: String,
+    val imageUrl: String,
+    val price: Int,
+    val title: String
+)
--- app/src/main/java/com/example/e_commerce_application/ui/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/e_commerce_application/ui/fragments/list/ListFragment.kt
@@ -19,7 +19,7 @@ class ListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentListBinding.inflate(inflater, container, false)
         val view = binding.root
