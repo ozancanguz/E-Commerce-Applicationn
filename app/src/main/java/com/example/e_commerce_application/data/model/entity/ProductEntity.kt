1b9d68c592059fd12fce417c8345312757f68a7d
Ozan <ozancangu24@gmail.com>
2023-03-29 22:30:10 +0300

    -room part ProductEntity.kt created

---
+++ app/src/main/java/com/example/e_commerce_application/data/model/entity/ProductEntity.kt
@@ -0,0 +1,13 @@
+package com.example.e_commerce_application.data.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "products_table" )
+data class ProductEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int=0,
+    val title: String,
+    val price: Int,
+    )
