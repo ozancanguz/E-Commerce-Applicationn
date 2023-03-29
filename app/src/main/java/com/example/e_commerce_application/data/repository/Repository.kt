7b09e0f0c767e0ff16c43fdbdd52cca43b3dcb16
Ozan <ozancangu24@gmail.com>
2023-03-29 18:36:47 +0300

    -repository created

--- app/src/main/java/com/example/e_commerce_application/data/repository/Repository.kt
+++ app/src/main/java/com/example/e_commerce_application/data/repository/Repository.kt
@@ -7,8 +7,8 @@ import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class Repository@Inject constructor(remoteDataSource: RemoteDataSource) {
+class Repository@Inject constructor(private val remoteDataSource: RemoteDataSource) {
 
-
+     val remote=remoteDataSource
 
 }
