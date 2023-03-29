7ff55e5bd38cc80b45995cbd99b614a40a26d904
Ozan <ozancangu24@gmail.com>
2023-03-29 22:36:57 +0300

    -database module created

---
+++ app/src/main/java/com/example/e_commerce_application/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.example.e_commerce_application.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.e_commerce_application.data.model.entity.ProductDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ProductDatabase::class.java,
+        "product_database"
+    ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ProductDatabase) = database.productDao()
+
+
+}
