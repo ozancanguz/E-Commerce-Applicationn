de405444458b1e53d2e20d322df7da082b66ffeb
Ozan <ozancangu24@gmail.com>
2023-03-30 00:10:50 +0300

    -data added to db.

--- app/src/main/java/com/example/e_commerce_application/ui/fragments/productDetails/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/e_commerce_application/ui/fragments/productDetails/ProductDetailsFragment.kt
@@ -1,14 +1,18 @@
 package com.example.e_commerce_application.ui.fragments.productDetails
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.e_commerce_application.R
+import com.example.e_commerce_application.data.model.entity.ProductEntity
 import com.example.e_commerce_application.databinding.FragmentProductDetailsBinding
 import com.example.e_commerce_application.util.Util.Companion.loadImage
+import com.example.e_commerce_application.viewmodel.ProductViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +23,8 @@ class ProductDetailsFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args:ProductDetailsFragmentArgs by navArgs()
+
+    private val productViewModel:ProductViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +37,9 @@ class ProductDetailsFragment : Fragment() {
         // getDetails
         getDetails()
 
+        // insert to db
+        insertDb()
+
 
         return view
 
@@ -41,6 +50,20 @@ class ProductDetailsFragment : Fragment() {
         binding.imageView.loadImage(args.details.imageUrl)
         binding.detailsPrice.text=args.details.price.toString()
         binding.detailsTitle.text=args.details.title
+    }
+
+
+    private fun insertDb(){
+        binding.addCart.setOnClickListener {
+            val image=binding.imageView.loadImage(args.details.imageUrl)
+            val price=binding.detailsPrice.text.toString().toInt()
+            val title=binding.detailsTitle.text.toString()
+
+            val newItem=ProductEntity(0,title,price)
+            productViewModel.insertProductEntity(newItem)
+            Log.d("hello", " $newItem")
+
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
