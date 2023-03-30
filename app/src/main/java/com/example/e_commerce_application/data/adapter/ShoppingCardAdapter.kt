11a1028fad4685f4a8d3fb816c6641532e2bafb5
Ozan <ozancangu24@gmail.com>
2023-03-30 16:53:41 +0300

    -Shopping card adapter created

---
+++ app/src/main/java/com/example/e_commerce_application/data/adapter/ShoppingCardAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.e_commerce_application.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.e_commerce_application.R
+import com.example.e_commerce_application.data.model.entity.ProductEntity
+import com.example.e_commerce_application.databinding.ShoppingCartItemRowLayoutBinding
+
+class ShoppingCardAdapter : RecyclerView.Adapter<ShoppingCardAdapter.ShoppingCardViewHolder>() {
+
+    inner class ShoppingCardViewHolder(val binding: ShoppingCartItemRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    private var productList = emptyList<ProductEntity>()
+
+    fun setData(newData: List<ProductEntity>) {
+        productList = newData
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingCardViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ShoppingCartItemRowLayoutBinding.inflate(inflater, parent, false)
+        return ShoppingCardViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+    override fun onBindViewHolder(holder: ShoppingCardViewHolder, position: Int) {
+        val currentProduct = productList[position]
+        val binding = holder.binding
+
+        binding.productNameTv.text = currentProduct.title
+        binding.productpriceTV.text = currentProduct.price.toString()
+        binding.productAmountTv.text = "1"
+    }
+}
