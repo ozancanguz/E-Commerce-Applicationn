3a402ce4030434c7a91e687ee449c32983757276
Ozan <ozancangu24@gmail.com>
2023-04-01 19:57:32 +0300

    -register activity designed

--- app/src/main/java/com/example/e_commerce_application/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/e_commerce_application/ui/register/RegisterActivity.kt
@@ -3,10 +3,17 @@ package com.example.e_commerce_application.ui.register
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.e_commerce_application.R
+import com.example.e_commerce_application.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
