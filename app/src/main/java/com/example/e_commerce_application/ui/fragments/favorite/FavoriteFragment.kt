808b0c0b94c2955c6cb3a56480110bbf0f108936
Ozan <ozancangu24@gmail.com>
2023-04-02 20:24:38 +0300

    -favorites displayed on favorites fragment

--- app/src/main/java/com/example/e_commerce_application/ui/fragments/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/e_commerce_application/ui/fragments/favorite/FavoriteFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_commerce_application.R
+import com.example.e_commerce_application.data.adapter.FavoritesAdapter
 import com.example.e_commerce_application.databinding.FragmentFavoriteBinding
+import com.example.e_commerce_application.viewmodel.FavoritesViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
     private var _binding: FragmentFavoriteBinding? = null
 
     private val binding get() = _binding!!
 
+    private val favadapter=FavoritesAdapter()
+
+    private val favoritesViewModel:FavoritesViewModel by viewModels()
 
 
 
@@ -24,10 +33,29 @@ class FavoriteFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
+        // setting up recyclerview
+        setupRv()
+
+        // observe live data and update ui
+        observeFavoriteLiveData()
+
         return view
 
 
 
+    }
+
+    private fun observeFavoriteLiveData() {
+        favoritesViewModel.getAllFavorites.observe(viewLifecycleOwner, Observer {
+            favadapter.setData(it)
+        })
+    }
+
+    private fun setupRv() {
+        binding.favRv.layoutManager=LinearLayoutManager(requireContext())
+        binding.favRv.adapter=favadapter
     }
 
 
