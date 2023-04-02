0090ef25dddbd134bae50a5fda2f9b5dc3a5cb2d
Ozan <ozancangu24@gmail.com>
2023-04-02 19:40:39 +0300

    -local data source favorites functions created

--- app/src/main/java/com/example/e_commerce_application/data/local/LocalDataSource.kt
+++ app/src/main/java/com/example/e_commerce_application/data/local/LocalDataSource.kt
@@ -3,9 +3,12 @@ package com.example.e_commerce_application.data.local
 import androidx.lifecycle.LiveData
 import com.example.e_commerce_application.data.model.entity.ProductDao
 import com.example.e_commerce_application.data.model.entity.ProductEntity
+import com.example.e_commerce_application.data.model.favorites.FavoritesDao
+import com.example.e_commerce_application.data.model.favorites.FavoritesEntity
 import javax.inject.Inject
 
-class LocalDataSource@Inject constructor(private val productDao: ProductDao) {
+class LocalDataSource@Inject constructor(private val productDao: ProductDao,private val
+favoritesDao: FavoritesDao) {
 
 
      fun getAllProduct():LiveData<List<ProductEntity>>{
@@ -25,6 +28,25 @@ class LocalDataSource@Inject constructor(private val productDao: ProductDao) {
     suspend fun deleteAllProduct(){
         return productDao.deleteAllProduct()
     }
+
+    fun getAllFavorites():LiveData<List<FavoritesEntity>>{
+        return favoritesDao.getAllFavorites()
+    }
+
+    suspend fun inserFavorite(favoritesEntity: FavoritesEntity){
+        return favoritesDao.insertFavorite(favoritesEntity)
+    }
+
+    suspend fun deleteFavorite(favoritesEntity: FavoritesEntity){
+        return favoritesDao.deleteSingleFavorite(favoritesEntity)
+    }
+
+    suspend fun deleteAllFavorites(){
+        return favoritesDao.deleteAllFavorites()
+    }
+
+
+
 
 
 
