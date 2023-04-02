0d1aead4fcc9e105baa4854579ea10b2dfc7a97a
Ozan <ozancangu24@gmail.com>
2023-04-02 20:13:16 +0300

    -favorites adapter created

---
+++ app/src/main/java/com/example/e_commerce_application/data/adapter/FavoritesAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.e_commerce_application.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.e_commerce_application.R
+import com.example.e_commerce_application.data.model.favorites.FavoritesEntity
+import com.example.e_commerce_application.databinding.FavItemRowLayoutBinding
+
+class FavoritesAdapter : RecyclerView.Adapter<FavoritesAdapter.FavoritesViewHolder>() {
+
+    inner class FavoritesViewHolder(private val binding: FavItemRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(favorite: FavoritesEntity) {
+            binding.favproductNameTv.text = favorite.favTitle
+            binding.favproductpriceTV.text = favorite.favPrice.toString()
+        }
+    }
+
+    var favoriteList = emptyList<FavoritesEntity>()
+
+    fun setData(newData: List<FavoritesEntity>) {
+        this.favoriteList = newData
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritesViewHolder {
+        val binding = FavItemRowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FavoritesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return favoriteList.size
+    }
+
+    override fun onBindViewHolder(holder: FavoritesViewHolder, position: Int) {
+        val currentFav = favoriteList[position]
+        holder.bind(currentFav)
+    }
+}
