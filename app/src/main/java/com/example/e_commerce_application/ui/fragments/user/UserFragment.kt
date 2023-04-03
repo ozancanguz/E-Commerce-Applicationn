d4881cb6bd3142b528a68fce7a9c76fbaeacf49b
Ozan <ozancangu24@gmail.com>
2023-04-03 19:02:17 +0300

    -user details screen designed

--- app/src/main/java/com/example/e_commerce_application/ui/fragments/user/UserFragment.kt
+++ app/src/main/java/com/example/e_commerce_application/ui/fragments/user/UserFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.e_commerce_application.R
 import com.example.e_commerce_application.databinding.FragmentUserBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class UserFragment : Fragment() {
@@ -14,6 +18,8 @@ class UserFragment : Fragment() {
     private var _binding: FragmentUserBinding? = null
 
     private val binding get() = _binding!!
+
+    private lateinit var auth:FirebaseAuth
 
 
 
@@ -26,8 +32,32 @@ class UserFragment : Fragment() {
         val view = binding.root
 
 
+        // init firebase auth
+        auth= Firebase.auth
+
+        // get current user e-mail
+        getUserEmail()
+
+        // seeFavorites
+        SeeFavorites()
 
         return view
+    }
+
+    private fun getUserEmail(){
+        val currentUserEmail= auth.currentUser?.email
+        binding.useremailtv.text=currentUserEmail
+    }
+
+    private fun SeeFavorites(){
+        binding.navFavbtn.setOnClickListener {
+            findNavController().navigate(R.id.action_userFragment_to_favoriteFragment)
+        }
+
+        binding.imageView3.setOnClickListener {
+            findNavController().navigate(R.id.action_userFragment_to_favoriteFragment)
+
+        }
     }
 
 
